b2c7c175afb2e37e9a7d8ce40d9e230b55387c54
N3ther <thenetherreactor@gmail.com>
2022-03-01 09:35:10 -0500

    fixed issue #2
    "Error: JavaFX runtime components are missing, and are required to run this application" is no longer seen on startup.

---
+++ src/main/java/net/oijon/ukrainebot/Main.java
@@ -0,0 +1,71 @@
+package net.oijon.ukrainebot;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main extends Application
+{
+	/*
+	 * Checks if all keys have been supplied. Used later to open a popup.
+	 */
+	public static Boolean hasAllKeys(Properties p) {
+		Boolean hasAllKeys = null;
+		
+		String ibmKey = p.getProperty("ibm-key");
+    	String discordKey = p.getProperty("discord-key");
+    	
+    	if (ibmKey != null) {
+    		if (discordKey != null) {
+    			hasAllKeys = true;
+    		}
+    	} else {
+    		hasAllKeys = false;
+    	}
+		return hasAllKeys;
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		/*
+    	 * loads properties
+    	 * 
+    	 * this class.getClassLoader() thing is needed because the config file is technically not
+    	 * in the build path (at least to my understanding), and as such this makes sure it
+    	 * actually loads.
+    	 */
+    	System.out.println("Looking for config.properties in config.properties");
+    	InputStream s = Main.class.getClassLoader().getResourceAsStream("config.properties");
+    	Properties properties = new Properties();
+    	try {
+			properties.load(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	Boolean hasAllKeys = Main.hasAllKeys(properties);
+    	
+    	Stage stage = new Stage();
+    	stage.setTitle("UkraineBot Panel");
+		stage.show();
+    	
+    	if (hasAllKeys) {
+    		
+    	} else {
+    		
+    	}
+		
+	}
+}
