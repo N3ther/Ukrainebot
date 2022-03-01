67e36a96ac2c02d55f1da001dacf786a6a2e10e0
N3ther <thenetherreactor@gmail.com>
2022-03-01 01:54:34 -0500

    partially implemented javafx
    doesnt work currently, but im tired as its 2am. will fix tomorrow.

--- src/main/java/net/oijon/ukrainebot/App.java
+++ src/main/java/net/oijon/ukrainebot/App.java
@@ -1,13 +1,73 @@
 package net.oijon.ukrainebot;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
+	/*
+	 * Checks if all keys have been supplied. Used later to open a popup.
+	 */
+	public static Boolean hasAllKeys(Properties p) {
+		Boolean hasAllKeys = null;
+		
+		String ibmKey = p.getProperty("ibm-key");
+    	String discordKey = p.getProperty("discord-key");
+    	
+    	if (ibmKey != null) {
+    		if (discordKey != null) {
+    			hasAllKeys = true;
+    		}
+    	} else {
+    		hasAllKeys = false;
+    	}
+		return hasAllKeys;
+	}
+	
+	@Override
+    public void start(Stage primaryStage)
     {
-        System.out.println( "Hello World!" );
+    	/*
+    	 * loads properties
+    	 * 
+    	 * this class.getClassLoader() thing is needed because the config file is technically not
+    	 * in the build path (at least to my understanding), and as such this makes sure it
+    	 * actually loads.
+    	 */
+    	System.out.println("Looking for config.properties in config.properties");
+    	InputStream s = App.class.getClassLoader().getResourceAsStream("config.properties");
+    	Properties properties = new Properties();
+    	try {
+			properties.load(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	Boolean hasAllKeys = hasAllKeys(properties);
+    	
+    	Stage stage = new Stage();
+    	stage.setTitle("UkraineBot Panel");
+		stage.show();
+    	
+    	if (hasAllKeys) {
+    		
+    	} else {
+    		
+    	}
+
     }
 }
